60996f7a66dddbc051107c171bed60cb0cf3bcc3
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2023-07-30 11:16:09 +0530

    Add CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { currentDate } from '@myorg/functions';
+import cors from'cors';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+app.use(cors({origin: '*'}));
 
 app.get('/customer', (req, res) => {
   res.send(`[ customer ] ${currentDate()}`);
